15c84241fffae8a22ddbf91bb3c780dd80cd70c9
“A-Bag <bagrowski.adam@gmail.com>
2018-01-29 19:30:57 +0100

    TrelloFacade

--- src/main/java/com/crud/tasks/trello/client/TrelloClient.java
+++ src/main/java/com/crud/tasks/trello/client/TrelloClient.java
@@ -1,13 +1,12 @@
 package com.crud.tasks.trello.client;
 
-import com.crud.tasks.domain.created.trello.card.CreatedTrelloCard;
+import com.crud.tasks.domain.created.trello.card.CreatedTrelloCardDto;
 import com.crud.tasks.domain.TrelloBoardDto;
 import com.crud.tasks.domain.TrelloCardDto;
 import com.crud.tasks.trello.config.TrelloConfig;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
@@ -42,9 +41,9 @@ public class TrelloClient {
         }
     }
 
-    public CreatedTrelloCard createNewCard(TrelloCardDto trelloCardDto) {
+    public CreatedTrelloCardDto createNewCard(TrelloCardDto trelloCardDto) {
         URI url = buildUrlForCreatingNewCard(trelloCardDto);
-        return restTemplate.postForObject(url, null, CreatedTrelloCard.class);
+        return restTemplate.postForObject(url, null, CreatedTrelloCardDto.class);
     }
 
     private URI buildUrlForGettingTrelloBoards() {
