8f192369b288460d13e25d646c604908bfb0b36d
“A-Bag <bagrowski.adam@gmail.com>
2018-02-16 21:48:22 +0100

    thymeleaf trello card email v2

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -1,8 +1,10 @@
 package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
+import com.crud.tasks.config.CompanyConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.thymeleaf.TemplateEngine;
 import org.thymeleaf.context.Context;
@@ -12,6 +14,9 @@ public class MailCreatorService {
 
     @Autowired
     private AdminConfig adminConfig;
+
+    @Autowired
+    private CompanyConfig companyConfig;
 
     @Autowired
     @Qualifier("templateEngine")
@@ -24,7 +29,8 @@ public class MailCreatorService {
         context.setVariable("button", "Visit website");
         context.setVariable("admin_name", adminConfig.getAdminName());
         context.setVariable("goodbye_message", "Best wishes, ");
-        context.setVariable("company_info", "The best company");
+        context.setVariable("company_info", companyConfig.getCompanyName() +
+                "   |   " + companyConfig.getCompanyEmail() + "   |   " + companyConfig.getCompanyPhone());
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 }
